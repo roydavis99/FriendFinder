e5397de449b2278f7ce432214f518d1f30e1dad4
Rex <roydavis99.rd@gmail.com>
2018-12-13 21:52:40 -0700

    bugs

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -2,12 +2,13 @@
 // need to create a question builder
 
 // call API for questions
+$(document).ready(function(){
 
+  function CreateQuestion(interval, question) {
+    //let quest = $("<div>").append($("<h3>").text(`Question ${interval}`));
 
-function CreateQuestion(interval, question){
-  //let quest = $("<div>").append($("<h3>").text(`Question ${interval}`));
+    $("#questions").append("<div> <h3>Question</h3></div>");
+  }
 
-  $("#questions").append("<div> <h3>Question</h3></div>");
-}
-
-CreateQuestion(2,"I am here");
+  CreateQuestion(2, "I am here");
+});
